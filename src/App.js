6aa4223ecf62a75d07408c4462e1ad703e866af9
takashi-morishita <2608to1265@gmail.com>
2020-11-27 09:42:39 +0900

    implement reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
   return (
-    <h1>hello</h1>
-  );
+    <Counter>
+   </Counter>
+  )
+};
+const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  const handlePlusButton = () => {
+    setCount(count + 1);
+  }
+
+  const handleMinusButton = () => {
+    setCount(count - 1);
+  }
+
+  return (
+    <>
+      <div>counter: {count}</div>
+      <button onClick={handlePlusButton}>+1</button>
+      <button onClick={handleMinusButton}>-1</button>
+    </>
+  )
 };
 
 export default App;
